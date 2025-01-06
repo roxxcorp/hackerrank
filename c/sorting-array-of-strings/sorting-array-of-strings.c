765dff22ca596ffe2b4ec3e0cd42eff5a4a6bcbe
antoine <antoinepro@roxxmail.fr>
2025-01-06 18:10:04 +0100

    Suppression d'un retour à la ligne nauséabond.

--- c/sorting-array-of-strings/sorting-array-of-strings.c
+++ c/sorting-array-of-strings/sorting-array-of-strings.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 int lexicographic_sort(const char* a, const char* b) {
 
     return 0;
@@ -24,7 +25,6 @@ int sort_by_length(const char* a, const char* b) {
 void string_sort(char** arr,const int len,int (*cmp_func)(const char* a, const char* b)){
 
 }
-
 
 int main()
 {
